e65fadbff5f8f7d71ed3a717ed16a0ab23b41a52
josesalvador735 <juliansalvador@protonmail.com>
2024-06-22 01:49:49 +0200

    added fonctions to save and get the files id's + path + name

--- Diskloud/DisKloud.Client/MainWindow.xaml.cs
+++ Diskloud/DisKloud.Client/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System.IO;
+using System.Reflection.PortableExecutable;
 using System.Runtime.InteropServices;
 using System.Text;
 using System.Windows;
@@ -39,6 +40,48 @@ namespace DisKloud.Client
             }
             return dates;
 
+        }
+
+        public void save_file_id()
+        {
+            if (File.Exists("file_ids.txt"))
+            {
+                Console.WriteLine("Deleting old file");
+                File.Delete("file_ids.txt");
+            }
+            else
+            {
+                Console.WriteLine("Creating new file.");
+            }
+            string[] files = Directory.GetFiles(Directory.GetCurrentDirectory(), "*", SearchOption.AllDirectories);
+            for (int i = 0; i < files.Length; i++)
+            {
+                File.AppendAllText("file_ids.txt", i + ";" + Directory.GetCurrentDirectory() + ";" + files[i].Replace(Directory.GetCurrentDirectory() + "\\" ,"") + "\n");
+                Console.WriteLine("done");
+            }
+        }
+
+        public void get_file_id()
+        {
+            string[] file;
+            StreamReader reader = File.OpenText("file_ids.txt");
+            string line = null;
+            while ((line = reader.ReadLine()) != null)
+            {
+                if (line.IndexOf(";", StringComparison.CurrentCultureIgnoreCase) > -1)
+                {
+                    file = line.Split(";");
+                    for (int i = 0; i < file.Length; i++)
+                    {
+                        Console.WriteLine(file[i]);
+                    }
+                }
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            save_file_id();
         }
     }
 }
